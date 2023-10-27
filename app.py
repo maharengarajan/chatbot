a217a5eaf6fc9b94b9a45895565274ece284155c
RENGA RAJAN K <maharengarajan46@gmail.com>
2023-10-27 21:04:49 +0530

    working smtp variables

--- app.py
+++ app.py
@@ -23,11 +23,26 @@ host = os.getenv("database_host_name")
 user_name = os.getenv("database_user_name")
 password = os.getenv("database_user_password")
 database = os.getenv("database_name")
+smtp_server = os.getenv("smtp_server")
+smtp_port = os.getenv("smtp_port")
+smtp_username = os.getenv("smtp_username")
+smtp_password = os.getenv("smtp_password")
+sender_email = os.getenv("sender_email")
+receiver_emails = os.getenv("receiver_emails")
+cc_email = os.getenv("cc_email")
 
-print(host)
-print(user_name)
-print(password)
-print(database)
+# print(host)
+# print(user_name)
+# print(password)
+# print(database)
+# print(smtp_server)
+# print(smtp_port)
+# print(smtp_username)
+# print(smtp_password)
+# print(sender_email)
+# print(receiver_emails)
+# print(cc_email)
+
 # Connection from Python to MySQL
 mydb = conn.connect(host=host,user=user_name,password=password,database=database)
 cursor = mydb.cursor()
@@ -171,6 +186,108 @@ def is_valid_email(email):
 
 def is_valid_contact_number(contact):
     return bool(re.match(r"^\+?\d{1,3}[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$", contact))
+
+
+# this API is responsible for selecting industries
+@app.route("/chatbot/new_client/user_details/industries", methods=["POST"])
+def industries():
+    try:
+        industries = {
+            "1": "Insurance",
+            "2": "Banking",
+            "3": "Finance",
+            "4": "IT",
+            "5": "Healthcare",
+            "6": "Internet",
+            "7": "Automobile",
+            "8": "Others",
+        }
+        data = request.get_json()
+        row_id = data.get("row_id")  # Get the user ID from the request
+
+        selected_options = data.get("selected_options", [])
+        selected_industries = [
+            industries[opt] for opt in selected_options if opt in industries
+        ]
+
+        industry_str = ",".join(selected_industries)  # Convert lists to strings
+
+        query = "UPDATE new_client SET INDUSTRY = %s WHERE ID = %s"
+        values = (industry_str, row_id)
+        cursor.execute(query, values)
+        mydb.commit()
+        logging.info(f"industries saved in database: {selected_industries}")
+        return jsonify({"selected_industries": selected_industries, "code": 200})
+    except Exception as e:
+        return jsonify(
+            {"message": "Internal server error.", "error": str(e), "code": 500}
+        )
+
+# this API is responsible for selecting verticals
+@app.route("/chatbot/new_client/user_details/industries/verticals", methods=["POST"])
+def verticals_new_client():
+    try:
+        verticals = {
+            "1": "ML/DS/AI",
+            "2": "Sales force",
+            "3": "Microsoft dynamics",
+            "4": "Custom app",
+            "5": "Others",
+        }
+
+        data = request.get_json()
+        row_id = data.get("row_id")  # Get the user ID from the request
+
+        selected_options = data.get("selected_options", [])
+        selected_verticals = [
+            verticals[opt] for opt in selected_options if opt in verticals
+        ]
+
+        vertical_str = ",".join(selected_verticals)
+
+        query = "UPDATE new_client SET VERTICAL = %s WHERE ID = %s"
+        values = (vertical_str, row_id)
+        cursor.execute(query, values)
+        mydb.commit()
+
+        logging.info(f"user selected verticals saved: {selected_verticals}")
+        return jsonify({"selected_verticals": selected_verticals, "code": 200})
+    except Exception as e:
+        return jsonify(
+            {"message": "Internal server error.", "error": str(e), "code": 500}
+        )
+
+# this API is responsible for selecting requirements
+@app.route("/chatbot/new_client/user_details/industries/verticals/requirement",methods=["POST"])
+def requirement():
+    try:
+        requirements = {
+            "1": "Start the project from scratch",
+            "2": "Require support from existing project",
+            "3": "Looking for some kind of solutions",
+            "4": "Others",
+        }
+
+        data = request.get_json()
+        selected_option = data.get("selected_option")
+        row_id = data.get("row_id")  # Get the user ID from the request
+
+        if selected_option in requirements:
+            selected_requirement = requirements[selected_option]
+
+            query = "UPDATE new_client SET REQUIREMENTS = %s WHERE ID = %s"
+            values = (selected_requirement, row_id)
+            cursor.execute(query, values)
+            mydb.commit()
+
+            logging.info(f"selected requirement: {selected_requirement}")
+            return jsonify({"selected_requirement": selected_requirement, "code": 200})
+        else:
+            return jsonify({"message": "Please choose a valid option.", "code": 400})
+    except Exception as e:
+        return jsonify(
+            {"message": "Internal server error.", "error": str(e), "code": 500}
+        )
 
 
     
